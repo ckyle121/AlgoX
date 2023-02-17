d09c9e710247754ecabdda7d95910b78b43eb228
ckyle121 <carolinekyle21@gmail.com>
2023-02-17 17:07:02 -0600

    two sum

---
+++ TwoSum.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class TwoSum {
+    public static int[] twoNumberSum(int[] array, int targetSum ){
+        int[] result = new int[2];
+
+        Set<Integer> set= new HashSet<>();
+
+        for (var num : array){
+            set.add(num);
+        }
+
+        for (var num: array){
+            if (set.contains(targetSum - num)){
+                result[0] = num;
+                result[1] = targetSum - num;
+            }
+        }
+
+        return result; 
+    }
+
+}
