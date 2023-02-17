d6327218d604d824bc014d543b5549c19244e939
ckyle121 <carolinekyle21@gmail.com>
2023-02-17 17:21:24 -0600

    Find Largest Three Numbers

--- FindThreeLargestNumbers.java
+++ FindThreeLargestNumbers.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class FindThreeLargestNumbers {
+public class FindThreeLargestNumbers {
+    public static int[] findThreeLargestNumbers(int[] array) {
+        // Write your code here.
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MIN_VALUE;
+        int mid = Integer.MIN_VALUE;
+
+        for (int i = 0; i < array.length; i++){
+            if (array[i] > max){
+                min = mid;
+                mid = max;
+                max = array[i];
+            } else if (array[i] > mid){
+                min = mid;
+                mid = array[i];
+            } else if (array[i] > min){
+                min = array[i];
+            }
+        }
+
+        return new int[] { min, mid, max };
+    }
 }
--- FirstNonRepeatingCharacter.java
+++ FirstNonRepeatingCharacter.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class FirstNonRepeatingCharacter {
+public class FirstNonRepeatingCharacter {
+
+    public int firstNonRepeatingCharacter(String string) {
+        // Write your code here.
+        for (int i = 0; i < string.length(); i++){
+            String ch = String.valueOf(string.charAt(i));
+            if (string.indexOf(ch) == string.lastIndexOf(ch)){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
