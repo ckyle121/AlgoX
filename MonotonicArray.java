fcf78ee6b468d9c7b15d98c1ae600b57f495c4e1
ckyle121 <carolinekyle21@gmail.com>
2023-02-17 17:24:52 -0600

    Monotonic Array

--- MonotonicArray.java
+++ MonotonicArray.java
@@ -1,2 +1,20 @@
 public class MonotonicArray {
+    public static boolean isMonotonic(int[] array) {
+
+        if (array.length <= 1){
+            return true;
+        }
+
+        boolean isDecreasing = array[0] > array[array.length - 1];
+
+        for (int i = 1; i < array.length; i++){
+            if (isDecreasing && array[i] > array[i -1]){
+                return false;
+            }
+            if (!isDecreasing && array[i] < array[i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
 }
