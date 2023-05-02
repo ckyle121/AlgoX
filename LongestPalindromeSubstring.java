4023406af8c7f9160442fb34b9b06f6157c28464
ckyle121 <carolinekyle21@gmail.com>
2023-05-02 12:46:12 -0500

    longest palindrome substring

--- LongestPalindromeSubstring.java
+++ LongestPalindromeSubstring.java
@@ -1,2 +1,33 @@
-public class LongestPalindromeSubstring {
+import java.util.*;
+
+class Program {
+    public static String longestPalindromicSubstring(String str) {
+        String longest = "";
+
+        for (int i = 0; i < str.length(); i++){
+            for (int j = i; j < str.length(); j++){
+                String sub = str.substring(i, j + 1);
+
+                if (sub.length() > longest.length() && isPalindrome(sub)){
+                    longest = sub;
+                }
+            }
+        }
+        return longest;
+    }
+
+    public static boolean isPalindrome(String str){
+        int left = 0;
+        int right = str.length() -1;
+
+        while (left < right){
+            if (str.charAt(left) != str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
 }
